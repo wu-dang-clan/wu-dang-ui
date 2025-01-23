42ac470565f1f1b5738e91ce43c7dc77c7d7d78c
jamooooong <102500033+jamooooong@users.noreply.github.com>
2025-01-23 20:47:25 +0900

    [ ✨ | #1 ] add icon svg file

--- src/shared/assets/icons/index.ts
+++ src/shared/assets/icons/index.ts
@@ -1,5 +1,41 @@
+import { ReactComponent as AccountCircle } from "./account_circle.svg";
+import { ReactComponent as Add } from "./add.svg";
+import { ReactComponent as ArrowBack } from "./arrow_back.svg";
+import { ReactComponent as ArrowFoward } from "./arrow_forward.svg";
+import { ReactComponent as Close } from "./close.svg";
+import { ReactComponent as Delete } from "./delete.svg";
+import { ReactComponent as Done } from "./done.svg";
+import { ReactComponent as Edit } from "./edit.svg";
+import { ReactComponent as Favorite } from "./favorite.svg";
+import { ReactComponent as FavoriteBorder } from "./favorite_border.svg";
+import { ReactComponent as Home } from "./home.svg";
 import { ReactComponent as Menu } from "./menu.svg";
+import { ReactComponent as Notifications } from "./notifications.svg";
+import { ReactComponent as Search } from "./search.svg";
+import { ReactComponent as Settings } from "./settings.svg";
+import { ReactComponent as Share } from "./share.svg";
+import { ReactComponent as Star } from "./star.svg";
+import { ReactComponent as StarBorder } from "./star_border.svg";
+import { ReactComponent as ThumbUp } from "./thumb_up.svg";
 
 export default {
+  accountCircle: AccountCircle,
+  add: Add,
+  arrowBack: ArrowBack,
+  arrowFoward: ArrowFoward,
+  close: Close,
+  delete: Delete,
+  done: Done,
+  edit: Edit,
+  favorite: Favorite,
+  favoriteBorder: FavoriteBorder,
+  home: Home,
   menu: Menu,
+  notifications: Notifications,
+  search: Search,
+  settings: Settings,
+  share: Share,
+  star: Star,
+  starBorder: StarBorder,
+  thumbUp: ThumbUp,
 };
