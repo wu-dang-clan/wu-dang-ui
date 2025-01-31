c071b0927b497e199b965f583575a7b5ed64a9b4
jamooooong <102500033+jamooooong@users.noreply.github.com>
2025-01-31 16:53:16 +0900

    [ ✨ | #3 ] implement callOut

---
+++ src/components/callOut/index.ts
@@ -0,0 +1,1 @@
+export { CallOut } from "./CallOut";
--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,10 @@ import "./shared/styles.css";
 export { icons };
 
 //export 할 컴포넌튿들을 모아놓은 index.ts 파일을 만들어서 export 한다.
+export * from "./components/alertDialog";
 export * from "./components/badge";
 export * from "./components/button";
+export * from "./components/callOut";
 export * from "./components/checkBox";
 export * from "./components/iconButton";
 export * from "./icon";
